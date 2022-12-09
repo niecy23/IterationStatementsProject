21b1090b1f2189d5b593fa183f630b9fb302b533
Chanise Hart <niecy23@gmail.com>
2022-12-09 12:35:48 -0700

    Added DONE

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -61,16 +61,16 @@ namespace IterationStatements
             Console.WriteLine("Decrease:");
 
 
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
-                // in your initializer set the value of i to 199
-                // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-                // AND as long as i is greater than or equal to 0
-                // Decrement i by 1
+            // Create a for loop - this will print the numbers in reverse order - from 200 to 1 - DONE!
+                // in your initializer set the value of i to 199 - DONE!
+                // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count) - DONE!
+                // AND as long as i is greater than or equal to 0 - DONE!
+                // Decrement i by 1 - DONE!
 
             //start for loop here
             for (var i = 199; i <= numbers.Count && i >= 0; i--)
             {
-                // place numbers[i] inside of the Console.WriteLine() method
+                // place numbers[i] inside of the Console.WriteLine() method - DONE!
                 Console.WriteLine($"{numbers[i]}");
             }
 
